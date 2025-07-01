3fa970e0ac10bc7d7bed6350c621b92c6e617be3
Kateryna <katerynawalls@proton.me>
2025-07-01 00:11:31 +0000

    fix(PT-67): adjust height on HeroSection and HeroFilters

--- src/features/heroSection/components/Hero/HeroFilters.tsx
+++ src/features/heroSection/components/Hero/HeroFilters.tsx
@@ -10,9 +10,13 @@ import { useState } from 'react';
 const HeroFilters = () => {
   const selectedAnimal = useAppSelector(selectAnimal);
   const [selectedDistrict, setSelectedDistrict] = useState<string>('');
+  const heightClass =
+    selectedAnimal === 'cat' ? 'max-h-[416px]' : 'max-h-[622px]';
 
   return (
-    <div className="relative bg-alabaster shadow-box rounded-[25px] xl:rounded-2xl pt-[27px] pb-[27px] px-4.5 xl:py-4 xl:px-8 w-full mx-auto flex flex-col  xl:gap-3.5 xl:mb-0 xl:-mt-212 z-10 left-1/2 -translate-x-1/2 min-h-[416px] xl:min-h-0 max-h-[622px]">
+    <div
+      className={`absolute bg-alabaster shadow-box rounded-[25px] xl:rounded-2xl py-[27px] px-4.5 xl:py-5 xl:px-8 xl:w-[1040px] mx-auto xl:max-h-[298px] flex flex-col xl:gap-3.5 xl:mb-0 xl:-mt-212 z-10 left-1/2 -translate-x-1/2 ${heightClass}`}
+    >
       <div className="flex flex-col xl:flex-row xl:justify-center gap-[13px] xl:gap-8 mb-[38px] xl:mb-0">
         <AnimalSelector />
         <DistrictSelector
--- src/features/heroSection/pages/HeroSection.tsx
+++ src/features/heroSection/pages/HeroSection.tsx
@@ -2,10 +2,21 @@ import HeroImage from '@/features/heroSection/components/Hero/HeroImage';
 import HeroFilters from '@/features/heroSection/components/Hero/HeroFilters';
 import DogWithBlob from '@/features/heroSection/components/Hero/HeroBlob';
 import HeroTitle from '@/features/heroSection/components/Hero/HeroTitle';
+import { useAppSelector } from '@/shared/hooks';
+import { selectAnimal } from '@/features/heroSection/hooks/heroSelectors';
 
 const HeroSection = () => {
+  const selectedAnimal = useAppSelector(selectAnimal);
+
+  const heightClass =
+    selectedAnimal === 'cat'
+      ? 'h-[662px] xl:max-h-165'
+      : 'h-[858px] xl:max-h-165';
+
   return (
-    <section className="relative w-full max-w-[375px] xl:max-w-[1280px] mx-auto min-h-[716px] xl:min-h-0 max-h-[922px] xl:max-h-165 rounded-b-[40px] z-10 -mt-[56px] xl:-mt-20 px-[15px] pt-0 xl:py-12 xl:px-30 xl:mb-40 bg-hero-gradient">
+    <section
+      className={`relative w-full max-w-[375px] xl:max-w-[1280px] mx-auto  rounded-b-[40px] z-10 -mt-[56px] xl:-mt-20 px-[15px] pt-0 xl:py-12 xl:px-30 xl:mb-40 bg-hero-gradient ${heightClass}`}
+    >
       <div className="relative z-10 flex flex-col xl:flex-row items-start xl:items-center xl:justify-between">
         <HeroTitle />
         <HeroImage />
